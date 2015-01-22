839793f06bb5c8d9536e3c284fa63c1fb470bf6c
MFilho <mauriliofilho22@gmail.com>
2015-01-22 02:09:46 -0200

    Creating repository for new project
    Creating project for download de content for studies in I.T

---
+++ PaperIT/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace PaperIT
+{
+	[Activity (Label = "PaperIT", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			var txtBook   = FindViewById<EditText> (Resource.Id.txtBook);
+			var btnSearch = FindViewById<Button> (Resource.Id.btnSearch);
+			var lswBooks = FindViewById<ListView> (Resource.Id.lswBooks);
+
+			btnSearch.Click += async (sender, e) => {
+				var itEbook_api = new Shared.itEbook_api();
+				var booksIT = await itEbook_api.GetAsync(txtBook.Text);
+				lswBooks.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItemSingleChoice, booksIT);
+
+			};
+		}
+	}
+}
+
+
---
+++ Shared/MyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+
+namespace Shared
+{
+	public class itEbook_api
+	{
+		public async Task<List<string>> GetAsync(string books)
+		{
+			string url = string.Format("http://it-ebooks-api.info/v1/search/");
+
+			var client = new HttpClient ();
+			client.DefaultRequestHeaders.Add ("User-Agent", "Other");
+
+			var response = await client.GetAsync (url);
+			var content = await response.Content.ReadAsStringAsync ();
+
+			var json = JArray.Parse (content);
+
+			var book = new List<string> ();
+			foreach (var item in json) {
+				var booksIT = item.Value<string> ("Title");
+				book.Add (booksIT);
+			}
+			return book;
+		}
+	}
+}
+
